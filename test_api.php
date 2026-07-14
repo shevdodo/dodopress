6bc7988e984efc2dbf908c787e626370e49976ee
shevdodo <shevdodo@gmail.com>
2026-07-14 17:22:42 +0700

    chore: remove unused API and database testing scripts

--- test_api.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$ch=curl_init('https://rajaongkir.komerce.id/api/v1/destination/province'); 
-curl_setopt($ch,CURLOPT_HTTPHEADER,['key: UNQgC3Uze4e080ee548ca35cjLgg6QIb']); 
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); 
-$r=curl_exec($ch); 
-echo $r;
--- test_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$c = new \App\Http\Controllers\CartController();
-$data = $c->index()->getData();
-echo json_encode($data['provinces']);
