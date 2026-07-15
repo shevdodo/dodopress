271e8443d6761a6b15d0c65541691a8fd71664d6
shevdodo <shevdodo@gmail.com>
2026-07-15 16:48:45 +0700

    feat: add admin dashboard interface for listing and managing user orders

--- resources/views/dashboard/orders/admin_index.blade.php
+++ resources/views/dashboard/orders/admin_index.blade.php
@@ -101,7 +101,7 @@
 
                         <div class="pt-1">
                             <a href="{{ route('superuser.orders.show', $order) }}" class="flex items-center justify-center w-full space-x-2 text-brand-600 font-bold text-sm bg-brand-50 hover:bg-brand-100 px-4 py-3 rounded-xl transition">
-                                <svg class="w-4.5 h-4.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
+                                <svg class="w-5 h-5 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
                                 <span>View Details</span>
                             </a>
                         </div>
